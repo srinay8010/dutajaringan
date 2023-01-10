1c222d94ea12bb1a4a09900364cb11e8bb28ad1f
Bryan <mhdalana211201@gmail.com>
2023-01-11 00:48:05 +0700

    mengubah struktur html & class css

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -1,16 +1,15 @@
+<?= $this->extend('partials/template'); ?>
+<?= $this->section('content'); ?>
+
 <!-- banner -->
-<div class="banner">
-  <div class="banner_bg">
-    <div class="container">
-      <div class="row"></div>
-    </div>
-  </div>
-</div>
+<section class="banner">
+  <div class="banner_bg"></div>
+</section>
 <!-- end banner -->
 
 <!-- about -->
-<div id="about" class="about">
-  <div class="container-fluid">
+<section id="about" class="about">
+  <div class="container">
     <div class="row d_flex">
       <div class="col-md-5">
         <div class="about_img">
@@ -19,110 +18,93 @@
       </div>
 
       <div class="col-md-7">
-        <div class="titlepage">
-          <h2>About <span class="blu">Company</span></h2>
+        <h2 class="titlepage">About <span class="blu">Company</span></h2>
 
-          <p>PT DUTA JARINGAN INDONESIA didirikan pada bulan Juni 2021, berlokasi di
-            Lagoon Avenue Bekasi Management Office Lantai G, Kawasan Grand Kamala \
-            Lagoon, Jl. Chandrabaga, Kota Bekasi, Jawa Barat, 17141. Kami bergerak di bidang
-            teknologi,jaringan, jasa, dan perawatan properti kantor, hotel, dan rumah dengan didukung tenaga kerja yang profesional.
-            Duta Jaringan berdedikasi memberikan pelayanan yang terbaik untuk memenuhi kebutuhan para customer.
-          </p>
+        <p>PT DUTA JARINGAN INDONESIA didirikan pada bulan Juni 2021, berlokasi di
+          Lagoon Avenue Bekasi Management Office Lantai G, Kawasan Grand Kamala \
+          Lagoon, Jl. Chandrabaga, Kota Bekasi, Jawa Barat, 17141. Kami bergerak di bidang
+          teknologi,jaringan, jasa, dan perawatan properti kantor, hotel, dan rumah dengan didukung tenaga kerja yang profesional.
+          Duta Jaringan berdedikasi memberikan pelayanan yang terbaik untuk memenuhi kebutuhan para customer.
+        </p>
 
-          <a class="read_more" href="<?= base_url('/about'); ?>">More About Us</a>
-        </div>
+        <a class="read_more" href="<?= base_url('/about'); ?>" role="button">More About Us</a>
       </div>
     </div>
   </div>
-</div>
+</section>
 <!-- end about -->
 
 <!-- choose  section -->
-<div class="choose ">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="titlepage">
-          <h2>Our <span class="blu"> Services</span></h2>
-        </div>
-      </div>
-    </div>
-  </div>
-
+<section class="choose">
   <div class="choose_bg">
     <div class="container">
       <div class="row">
-        <div class="col-md-12">
-          <div class="row">
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 padding_right">
-              <ul class="easy">
-                <li class="active"><a href="#">Construction</a></li>
-                <li><a href="#">Implementation</a></li>
-                <li><a href="#">Maintenance & Service</a></li>
-                <li><a href="#">Rental</a></li>
-                <li><a href="#">Web Development</a></li>
-              </ul>
-            </div>
+        <div class="titlepage">
+          <h2>Our <span class="white"> Services</span></h2>
+        </div>
 
-            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 padding_left0">
-              <div class="choose_box">
-                <i><img src="<?= base_url('./images/admin.png'); ?>" alt="#" /></i>
-                <h3>Rental</h3>
-                <p>
-                  PT DUTA JARINGAN INDONESIA menawarkan jasa untuk membangun dan mendesain sebuah website untuk memenuhi kebutuhan bisnis Anda.
-                </p>
-              </div>
-            </div>
+        <div class="col-md-3 col-sm-12 pe-0">
+          <ul class="easy">
+            <li class="active"><a href="#">Construction</a></li>
+            <li><a href="#">Implementation</a></li>
+            <li><a href="#">Maintenance & Service</a></li>
+            <li><a href="#">Rental</a></li>
+            <li><a href="#">Web Development</a></li>
+          </ul>
+        </div>
+
+        <div class="col-md-9 col-sm-12 ps-0">
+          <div class="choose_box">
+            <i><img src="<?= base_url('./images/admin.png'); ?>" alt="#" /></i>
+            <h3>Rental</h3>
+            <p>
+              PT DUTA JARINGAN INDONESIA menawarkan jasa untuk membangun dan mendesain sebuah website untuk memenuhi kebutuhan bisnis Anda.
+            </p>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+</section>
 <!-- end choose  section -->
 
 <!-- request -->
-<div id="contact" class="request">
+<section id="contact" class="request py-5">
   <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="titlepage">
-          <h2>Request <span class="white">A call Back</span></h2>
-        </div>
-      </div>
-    </div>
+    <h1 class="titlepage mb-5">Request <span class="white">A call Back</span></h1>
 
     <div class="row">
-      <div class="col-md-6">
-        <form id="request" class="main_form">
-          <div class="row">
-            <div class="col-md-12 ">
-              <input class="contactus" placeholder="Full Name" type="type" name="Full Name">
-            </div>
+      <contact-form class="col-md-6">
+        <form id="request" class="main_form" action="" method="POST">
+          <input class="contactus" placeholder="Full Name" type="type" name="Full Name">
+          <input class="contactus" placeholder="Email" type="type" name="Email">
+          <input class="contactus" placeholder="Phone Number" type="type" name="Phone Number">
+          <textarea class="textarea" placeholder="Message" type="type" Message="Name" rows="4">Message</textarea>
 
-            <div class="col-md-12">
-              <input class="contactus" placeholder="Email" type="type" name="Email">
-            </div>
-
-            <div class="col-md-12">
-              <input class="contactus" placeholder="Phone Number" type="type" name="Phone Number">
-            </div>
-
-            <div class="col-md-12">
-              <textarea class="textarea" placeholder="Message" type="type" Message="Name">Message </textarea>
-            </div>
-
-            <div class="col-md-12">
-              <button class="send_btn">Send</button>
-            </div>
-          </div>
+          <button class="send_btn">Send</button>
         </form>
-      </div>
+      </contact-form>
 
       <div class="col-md-6">
         <div id="map"></div>
       </div>
     </div>
   </div>
-</div>
+</section>
 <!-- end request -->
+
+<script>
+class ContactForm extends HTMLElement {
+  connectedCallback() {
+    this.render()
+  }
+
+  render() {}
+}
+
+customElements.define('contact-form', ContactForm)
+
+document.querySelector('contact-form').style.display = 'block'
+</script>
+
+<?= $this->endSection(); ?>
