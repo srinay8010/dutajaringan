faae9d57d92b2bbf1fb952ac9dd6e19b5ec80a2b
Bryan <mhdalana211201@gmail.com>
2023-01-11 00:42:44 +0700

    memisahkan loader

---
+++ app/Views/partials/loader.php
@@ -0,0 +1,5 @@
+    <!-- loader  -->
+    <div class="loader_bg">
+      <div class="loader"><img src="<?= base_url('./images/loading.gif'); ?>" alt="#" /></div>
+    </div>
+    <!-- end loader -->
