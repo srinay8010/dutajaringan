e342f7c60f823232f54b2c17be60b79fb0953fdc
Bryan <mhdalana211201@gmail.com>
2023-01-11 00:46:15 +0700

    memperbaiki struktur html & class css

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,71 +1,53 @@
+<?= $this->extend('partials/template'); ?>
+<?= $this->section('content'); ?>
+
 <!-- About -->
 <!-- Upper -->
-<div class="about_page">
-  <div class="up_bg">
-    <div class="upper">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="titlepage">
-              <h1>About</h1>
-            </div>
-          </div>
+<main class="about_page">
+  <section class="up_bg pt-5">
+    <div class="container pb-4">
+      <h1 class="pb-4 text-center">About</h1>
 
-          <div class="col-md-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ex eleifend mi mattis dapibus. Praesent lobortis eu quam sit amet aliquet. Integer mollis auctor malesuada.
-              Pellentesque aliquet, lacus nec ultricies dictum, augue arcu molestie elit, id efficitur lectus dui et mauris. Sed at mattis velit. Proin facilisis, lacus a viverra mattis, dui magna
-              varius nisl, quis accumsan justo lectus maximus sapien. In vel pharetra erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin tincidunt felis magna, ut dictum leo
-              vestibulum eget. Fusce ut purus et augue pellentesque ornare. <br>
-            </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ex eleifend mi mattis dapibus. Praesent lobortis eu quam sit amet aliquet. Integer mollis auctor malesuada.
+        Pellentesque aliquet, lacus nec ultricies dictum, augue arcu molestie elit, id efficitur lectus dui et mauris. Sed at mattis velit. Proin facilisis, lacus a viverra mattis, dui magna
+        varius nisl, quis accumsan justo lectus maximus sapien. In vel pharetra erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin tincidunt felis magna, ut dictum leo
+        vestibulum eget. Fusce ut purus et augue pellentesque ornare. <br>
+      </p>
 
-            <p>
-              Phasellus tincidunt viverra gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus iaculis, dui euismod fermentum tincidunt, neque metus
-              interdum leo, ac dignissim arcu lectus at ex. In lacinia neque vel sem dignissim rutrum. Curabitur ac vestibulum nibh. Nunc tristique ut libero vitae blandit. Curabitur leo nunc,
-              bibendum et laoreet in, rhoncus ut metus. Vivamus sit amet imperdiet purus, nec varius justo.
-            </p>
-          </div>
-        </div>
-      </div>
+      <p>
+        Phasellus tincidunt viverra gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus iaculis, dui euismod fermentum tincidunt, neque metus
+        interdum leo, ac dignissim arcu lectus at ex. In lacinia neque vel sem dignissim rutrum. Curabitur ac vestibulum nibh. Nunc tristique ut libero vitae blandit. Curabitur leo nunc,
+        bibendum et laoreet in, rhoncus ut metus. Vivamus sit amet imperdiet purus, nec varius justo.
+      </p>
     </div>
-  </div>
+  </section>
   <!-- End of Upper -->
 
   <!-- Banner -->
-  <div class="banner">
+  <section class="banner">
     <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="banner">
-            <img src="<?= base_url('./images/banner.jpg'); ?>">
-          </div>
-        </div>
-      </div>
+      <img src="<?= base_url('./images/banner.jpg'); ?>">
     </div>
-  </div>
+  </section>
   <!-- End of Banner -->
 
   <!-- Mid -->
-  <div class="mid_bg">
-    <div class="mid">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="desc_image">
-              <p>Phasellus tincidunt viverra gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus iaculis, dui euismod fermentum tincidunt, neque
-                metus interdum leo, ac dignissim arcu lectus at ex. In lacinia neque vel sem dignissim rutrum. Curabitur ac vestibulum nibh. Nunc tristique ut libero vitae blandit. Curabitur leo nunc,
-                bibendum et laoreet in, rhoncus ut metus. Vivamus sit amet imperdiet purus, nec varius justo. <br>
-              </p>
+  <section class="mid_bg pb-5">
+    <div class="container">
+      <div class="desc_image">
+        <p>Phasellus tincidunt viverra gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus iaculis, dui euismod fermentum tincidunt, neque
+          metus interdum leo, ac dignissim arcu lectus at ex. In lacinia neque vel sem dignissim rutrum. Curabitur ac vestibulum nibh. Nunc tristique ut libero vitae blandit. Curabitur leo nunc,
+          bibendum et laoreet in, rhoncus ut metus. Vivamus sit amet imperdiet purus, nec varius justo. <br>
+        </p>
 
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ex eleifend mi mattis dapibus. Praesent lobortis eu quam sit amet aliquet. Integer mollis auctor malesuada.
-                Pellentesque aliquet, lacus nec ultricies dictum, augue arcu molestie elit, id efficitur lectus dui et mauris. Sed at mattis velit. Proin facilisis, lacus a viverra mattis, dui magna
-                varius nisl, quis accumsan justo lectus maximus sapien. In vel pharetra erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin tincidunt felis magna, ut dictum leo
-                vestibulum eget. Fusce ut purus et augue pellentesque ornare.</p>
-            </div>
-          </div>
-        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ex eleifend mi mattis dapibus. Praesent lobortis eu quam sit amet aliquet. Integer mollis auctor malesuada.
+          Pellentesque aliquet, lacus nec ultricies dictum, augue arcu molestie elit, id efficitur lectus dui et mauris. Sed at mattis velit. Proin facilisis, lacus a viverra mattis, dui magna
+          varius nisl, quis accumsan justo lectus maximus sapien. In vel pharetra erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin tincidunt felis magna, ut dictum leo
+          vestibulum eget. Fusce ut purus et augue pellentesque ornare.</p>
       </div>
     </div>
-  </div>
+  </section>
   <!-- End of Mid -->
-</div>
+</main>
 <!-- End of About Page -->
+<?= $this->endSection(); ?>
