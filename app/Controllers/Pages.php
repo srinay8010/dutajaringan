af0d9dcd0d569422eb9ddb24d7810d52291d2ad9
Bryan <mhdalana211201@gmail.com>
2023-01-27 19:44:26 +0700

    memperbaiki route link

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -24,18 +24,18 @@ class Pages extends BaseController
     public function services($page = 'services')
     {
         $data['title'] = ucfirst($page);
-        return view('pages/services' . $page);
+        return view('pages/' . $page);
     }
 
     public function products($page = 'products')
     {
         $data['title'] = ucfirst($page);
-        return view('pages/products' . $page);
+        return view('pages/' . $page);
     }
 
     public function test($page = 'test')
     {
         $data['title'] = ucfirst($page);
-        return view('pages/test'. $page);
+        return view('pages/'. $page);
     }
 }
