dc9b5b54d483f65594868558cb39bdce3bbbb26c
Bryan <mhdalana211201@gmail.com>
2023-01-31 19:55:08 +0700

    membuat looping pemanggilan data product

--- app/Views/pages/products.php
+++ app/Views/pages/products.php
@@ -2,195 +2,21 @@
 <?= $this->section('content'); ?>
 
 <main class="container">
-  <h1>hello world</h1>
+  <h1 class="my-4">Products</h1>
 
-  <div class="row position-relative" style="z-index: -1;">
-    <div class="col-md-4">
+  <div class="row row-cols-5 position-relative" style="z-index: -1;">
+    <?php foreach($products as $product) : ?>
+    <div class="col">
       <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
+        <img src="<?= base_url('/images/' . $product['gambar_produk']); ?>" class="card-img-top" alt="<?= $product['nama_produk']; ?>">
         <div class="card-body">
-          <h3 class="card-title">Seagate Skyhawk</h3>
-          <span class="badge text-bg-info">Harddisk</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h3 class="card-title fs-6"><?= $product['nama_produk']; ?></h3>
+          <span class="badge text-bg-info m-2 position-absolute top-0 bg-opacity-50" style="left: 0;"><?= $product['jenis_produk']; ?></span>
+          <p class="card-text">Rp. <?= $product['harga_produk']; ?></p>
         </div>
       </div>
     </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">WD Purple</h3>
-          <span class="badge text-bg-info">Harddisk</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Hikvision</h3>
-          <span class="badge text-bg-info">CCTV</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Panasonic</h3>
-          <span class="badge text-bg-info">CCTV</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">ACTi</h3>
-          <span class="badge text-bg-info">CCTV</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">AMTEK</h3>
-          <span class="badge text-bg-info">CCTV</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Belden</h3>
-          <span class="badge text-bg-info">Cables</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">ANENG</h3>
-          <span class="badge text-bg-info">Grounding</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Dekko</h3>
-          <span class="badge text-bg-info">Grounding</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">KYORITSU</h3>
-          <span class="badge text-bg-info">Grounding</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Fluke</h3>
-          <span class="badge text-bg-info">Grounding</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Sanwa</h3>
-          <span class="badge text-bg-info">Grounding</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">SEW</h3>
-          <span class="badge text-bg-info">Grounding</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Hikvision</h3>
-          <span class="badge text-bg-info">NVR</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Panasonic</h3>
-          <span class="badge text-bg-info">NVR</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">ACTi</h3>
-          <span class="badge text-bg-info">NVR</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="card mb-3">
-        <img src="<?= base_url('/images/img2.png'); ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">AMTEK</h3>
-          <span class="badge text-bg-info">NVR</span>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
-    </div>
+    <?php endforeach; ?>
 
   </div>
 </main>
